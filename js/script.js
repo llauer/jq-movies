fab559535d622fd12eec03f9dad9a5c50f97c3ef
Larry Lauer <larry.lauer.3d@gmail.com>
2019-03-11 15:31:39 -0700

    1-8 learning jQuery

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+  var $movies = $('.movies');
+  $.ajax('http://www.omdbapi.com/?s=star%20wars&apikey=f560962b&y=&plot=short&r=json', {
+    dataType: 'json',
+    success: function(result) {
+      // console.log(result);
+      $.each(result.Search, function(i, movie) {
+        $movies.append('<li>Title: ' + movie.Title + ', Year: ' + movie.Year + '</li>');
+      });
+    }
+  });
+});
+
+//   })
+//   .then(function(movies) {
+//     $.each(movies, function(i, movie) {
+//       $movies.append('<li>Title: ' + movie.Title + ', Year: ' + movie.Year + '</li>');
+//     });
+//   });
+// });
